107fdd766637e8d95ec76a917c77c16e131b3234
Marc Bellavista <marcbellavistafabrega@gmail.com>
2021-03-22 23:41:17 +0100

    fix on db connect

--- Server/api/getFormatter/pokemonFormatter.socket.js
+++ Server/api/getFormatter/pokemonFormatter.socket.js
@@ -4,12 +4,15 @@ function getPokemons(){
     var pokemons = []
     for (let index = 0; index < 6; index++) {
             
-        let pokemon = pokemonDB.findById(Math.floor(Math.random() * 151));
-        
-        pokemons.push(JSON.stringify(pokemon));
+        pokemonDB.findById(Math.floor(Math.random() * 151),(error, result) =>{
+            if(error!= null){
+                pokemons.push(JSON.stringify(result));
 
-        console.log(pokemon);
-               
+                console.log("Pokemon: " + result);
+            }else{
+                console.log(error);
+            }
+        });           
                
     }
     return pokemons;
