10cdbcc31c68a036bcd8bc113d1272c35c363079
BellaCs <marcbellavistafabrega@gmail.com>
2021-03-23 18:31:55 +0100

    apires config and functionally

--- Server/api/routes/pokemonGameRoutes.js
+++ Server/api/routes/pokemonGameRoutes.js
@@ -1,7 +1,9 @@
 
 module.exports = function (app){
+    var getPokemons = require("../controllers/pokemonController")
 
-    app.route('/pokemons');
+    app.route('/pokemons')
+        .get(getPokemons.get_six_pokemon);
 
     app.route('/pokemon/:pokemonId');
 
--- Server/api/server.js
+++ Server/api/server.js
@@ -1,7 +1,16 @@
+const bodyParser = require("body-parser");
+
 var express = require("express"),
     app = express(),
-    port = process.env.PORT || 3000;
+    port = process.env.PORT || 3000,
+    bodyparser = require('body-parser');
 
+
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.json());
+
+var routes = require('./routes/pokemonGameRoutes');
+routes(app);
 
 app.listen(port);
 
