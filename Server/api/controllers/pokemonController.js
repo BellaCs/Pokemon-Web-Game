e3fd3ea080686889f14ed98ad3ffacd4a8a6c9a1
BellaCs <marcbellavistafabrega@gmail.com>
2021-03-23 20:40:03 +0100

    added headers

--- Server/api/controllers/pokemonController.js
+++ Server/api/controllers/pokemonController.js
@@ -2,12 +2,15 @@ var pokemonFormatter = require("../getFormatter/pokemonFormatter.socket");
 
 exports.get_six_pokemon = function(req, res){
     let pokemons;
+    res.headers.add("Access-Control-Allow-Origin", "*");
     pokemonFormatter.getPokemons(response => {
         pokemons = response;
     });
     if(pokemons != undefined){
+        
         res.json(pokemons);
     }else{
+        
         res.send("error");
     }
     return;
@@ -15,6 +18,7 @@ exports.get_six_pokemon = function(req, res){
 
 exports.read_a_pokemon = function(req, res){
     let pokemon = pokemonFormatter.getPokemonById(req.params.pokemonId);
+    res.headers.add("Access-Control-Allow-Origin", "*");
     if(pokemon != undefined){
         res.json(pokemon);
     }else{
