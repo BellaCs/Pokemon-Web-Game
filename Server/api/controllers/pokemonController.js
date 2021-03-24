b7f168e66d8925924ab3cffd6cae66d2c92863af
Marc Bellavista <marcbellavistafabrega@gmail.com>
2021-03-24 17:49:07 +0100

    .

--- Server/api/controllers/pokemonController.js
+++ Server/api/controllers/pokemonController.js
@@ -2,7 +2,7 @@ var pokemonFormatter = require("../getFormatter/pokemonFormatter.socket");
 
 exports.get_six_pokemon = function(req, res){
     let pokemons;
-    res.headers.add("Access-Control-Allow-Origin", "*");
+    res.addTrailers({"Access-Control-Allow-Origin" : "*"});
     pokemons = pokemonFormatter.getPokemons;
     if(pokemons != undefined){
         res.json(pokemon);
