75df0572ce9e42c89acec89d5b1f4b6fccaa74d3
Daniel Prieto <dprieto8@boscdelacoma.cat>
2021-03-24 18:35:37 +0100

    Creades funcions de derrota, i desconnexió

--- Server/Socket/socket.server.js
+++ Server/Socket/socket.server.js
@@ -7,7 +7,7 @@ const io=require("socket.io")(httpServer, {
     }
 });
 var partidaApi = {
-    "partida_id": "1",
+    "partida_id": "2",
     "jugador_1": "0",
     "jugador_1_pokemons": [
         {
@@ -62,23 +62,32 @@ io.on("connection", socket =>  {
                console.log("Segon jugador connectat");
         }
     });
+
     socket.on("ataque", function(msg, callback){
         //Generar atacar(msg) en api
         msg.daño_final = daño;
         socket.emit("atacado" + msg.partida_id, msg, function(){});
     });
+
     socket.on("cambiar", function(msg, callback){
         //Generar cambiar() en api
         msg.cambio = cambio;
         socket.emit("cambiado" + msg.partida_id, msg, function(){});
     });
+
     socket.on("derrota", function(msg, callback){
         //Enviar partida a DB
         partidas[msg.partida_id] = null;
         socket.emit("FIN" + msg.partida_id, msg.ganador, function(){});
     });
-    socket.on('disconnect', function(){
-        
+
+
+    socket.on('disconnect', function(msg){
+        partidas.forEach(element => {
+            if  (msg.jugador_id == element.jugador_1 || msg.jugador_id == element.jugador_2){
+                socket.emit("FIN" + msg.partida_id, "cualquiera", function(){});
+            }
+        });
     });
 });
 
