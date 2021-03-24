fa99d6b4007928d0fe0e40c826e84f20431cf22c
Marc Bellavista <marcbellavistafabrega@gmail.com>
2021-03-24 17:40:03 +0100

    try without headers

--- Server/api/controllers/pokemonController.js
+++ Server/api/controllers/pokemonController.js
@@ -14,7 +14,7 @@ exports.get_six_pokemon = function(req, res){
 
 exports.read_a_pokemon = function(req, res){
     let pokemon = pokemonFormatter.getPokemonById(req.params.pokemonId);
-    res.headers.add("Access-Control-Allow-Origin", "*");
+    //res.header.add("Access-Control-Allow-Origin", "*");
     if(pokemon != undefined){
         res.json(pokemon);
     }else{
