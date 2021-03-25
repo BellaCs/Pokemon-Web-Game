c84252a92bf557e524c9b4134bf00ce1ebe2287d
Marc Bellavista <marcbellavistafabrega@gmail.com>
2021-03-25 15:18:40 +0100

    fixin api try 3

--- Server/api/getFormatter/pokemonFormatter.socket.js
+++ Server/api/getFormatter/pokemonFormatter.socket.js
@@ -43,7 +43,7 @@ exports.getPokemonByIdToClient = (pokemonId, response) => {
                                             movementFormatter.getMovementToClient(posibleMoves[3], (error, result) => {
                                                 if (error == null) {
                                                     pokemon.atacs.push(result);
-                                                    response(JSON.parse(pokemon));
+                                                    response(pokemon);
                                                 } else {
                                                     console.log(error);
                                                     response(error);
