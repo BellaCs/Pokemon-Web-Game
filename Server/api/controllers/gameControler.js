15b02eb73d227661884d1c80906776e4962febdb
Marc Bellavista <marcbellavistafabrega@gmail.com>
2021-03-25 19:46:33 +0100

    post fix game 15

--- Server/api/controllers/gameControler.js
+++ Server/api/controllers/gameControler.js
@@ -4,7 +4,7 @@ exports.create_game = (req, res) => {
     console.log(req.body);
     let gameInfo = Buffer.from(JSON.stringify(req.body));
     if(gameInfo == null){res.sendStatus(400)}
-    game.create(JSON.stringify(gameInfo), result =>{
+    game.create(gameInfo, result =>{
         if (result != undefined) {
             let gameId = {
                 "gameId" : result
