bd06d2cc594c3d2f531f4f890ab3c6f6750cebb8
Marc Bellavista <marcbellavistafabrega@gmail.com>
2021-03-25 17:24:15 +0100

    api request

--- Server/api/controllers/gameControler.js
+++ Server/api/controllers/gameControler.js
@@ -8,7 +8,7 @@ exports.create_game = (req, res) => {
             let gameId = {
                 "gameId" : result
             }
-            res(gameId);
+            res.end(gameId);
         } else {
             res.sendStatus(404);
         }
