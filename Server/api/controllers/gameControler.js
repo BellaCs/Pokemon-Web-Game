27598521a0562545b52748335c2912e56de1f786
Marc Bellavista <marcbellavistafabrega@gmail.com>
2021-03-25 17:06:26 +0100

    fix api

--- Server/api/controllers/gameControler.js
+++ Server/api/controllers/gameControler.js
@@ -1,14 +1,10 @@
 const game = require("../models/game.database");
 
 exports.create_game = (req, res) => {
-    let gameInfo = game(req.body);
+    let gameInfo = req.body;
     if(gameInfo == null){res.sendStatus(400)}
     game.create(gameInfo, result =>{
         if (result != undefined) {
-            let gameId = {
-                "gameId" : result
-            }
-            res.setHeader("Access-Control-Allow-Origin", "*");
             res.json(gameId);
         } else {
             res.sendStatus(404);
