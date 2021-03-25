719b74373a2c314a52784bffd50348273a030a25
Marc Bellavista <marcbellavistafabrega@gmail.com>
2021-03-25 19:44:33 +0100

    post fix game 5

--- Server/api/controllers/gameControler.js
+++ Server/api/controllers/gameControler.js
@@ -2,7 +2,7 @@ const game = require("../models/game.database");
 
 exports.create_game = (req, res) => {
     console.log(req.body);
-    let gameInfo = req.body;
+    let gameInfo = Buffer.from(JSON.stringify(req.body));
     if(gameInfo == null){res.sendStatus(400)}
     game.create(JSON.stringify(gameInfo), result =>{
         if (result != undefined) {
--- Server/api/controllers/playerController.js
+++ Server/api/controllers/playerController.js
@@ -2,7 +2,7 @@ const player = require("../models/player.database");
 
 exports.create_player = function(req, res){
     console.log(req.body);
-    let playerInfo = req.body;
+    let playerInfo = Buffer.from(JSON.stringify(req.body));
     if(playerInfo == null){res.sendStatus(400)}
     player.create(playerInfo, result =>{
         if (result != undefined) {
